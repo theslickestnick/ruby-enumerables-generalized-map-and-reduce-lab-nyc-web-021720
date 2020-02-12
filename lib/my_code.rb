a6da9c636a695e4e8564cbf2d9f016564ed40d5a
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 22:50:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,10 @@
 require "pry"
 
-source_array.map do |element|
-  element * -1
-  element
-  element * 2
-  element * element
+def my_own_map(source_array)
+  yield(source_array)
 end
+
+source_array.map {|array| array * -1}
+source_array.map {|array| array}
+source_array.map {|array| array * 2}
+source_array.map {|array| array * array}
