5fe1bfb5429f07536d056600805f141095f95947
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 23:09:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,11 @@
 require "pry"
 
 def map(array)
-  new_array = []
+  new = []
   counter = 0
   while counter < array.size do
     new.push(yield(array[counter]))
   counter += 1
   end
-  new_array
+  new
 end
