842d485447c2c3c03e42e988e493154e72afcd50
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-12 23:23:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+require "pry"
+
+def map(array)
+  new = []
+  counter = 0
+  while counter < array.size do
+    new.push(yield(array[counter]))
+  counter += 1
+  end
+  new
+end
+
+def reduce(array, sv = nil)
+  if sv
+    sum = sv
+    counter = 0
+  else
+    sum = array[0]
+    counter = 1
+  end
+  while counter < array.size do
+    sum = yield(sum, array[counter])
+    counter +=1
+  end
+  sum
+end
